8a2d01335eb667f5d64ec8499c5907d4be40f4de
Yana <yana.ubushaeva@gmail.com>
2026-04-29 16:27:01 +0300

    kafka update

---
+++ src/main/java/digital/zil/hl/module1/configuration/JacksonConfig.java
@@ -0,0 +1,20 @@
+package digital.zil.hl.module1.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return mapper;
+    }
+}
