87e61a0fc3d1d2a4367c4e54b81c06e0505cd4b4
Yanaub <Yana.ubushaeva@gmail.com>
2026-05-06 17:49:35 +0300

    .

--- src/main/java/digital/zil/hl/module1/controller/CrashController.java
+++ src/main/java/digital/zil/hl/module1/controller/CrashController.java
@@ -2,15 +2,22 @@ package digital.zil.hl.module1.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/internal")
 public class CrashController {
 
     @PostMapping("/crash")
     public ResponseEntity<String> crash() {
-        System.err.println("CRASH ENDPOINT CALLED - Exiting JVM");
-        System.exit(1);
-        return ResponseEntity.ok("Should not reach here");
+        new Thread(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ignored) {
+            }
+            System.exit(1);
+        }).start();
+        return ResponseEntity.ok("core service will stop");
     }
 }
